8409b30639eee1e69ca7a9eb837023d8a1806518
Shivam-Thacker <38309239+Shivam-Thacker@users.noreply.github.com>
2020-01-01 16:08:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wotnot',
+    packages=['wotnot'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='https://github.com/Shivam-Thacker/wotnot',
+    author='Shivam',
+    author_email='shivamthacer05@gmail.com',
+    keywords=['pip','wotnot','private','directory']
+)
+
